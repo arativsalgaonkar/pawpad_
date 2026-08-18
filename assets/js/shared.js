1543a9e963b7ecd7bf6afad625c3bb9744a3720c
arati <arati@123>
2026-08-18 10:27:39 +0530

    updated website with contact info

--- assets/js/shared.js
+++ assets/js/shared.js
@@ -351,7 +351,7 @@ function Marquee({ items }) {
       `));
 }
 const WA_NUMBER = "918885349267";
-const WA_MESSAGE = "Hello, I would like to know more about your services.";
+const WA_MESSAGE = "Hii! 🐾 I saw your website and wanted to ask about your services for my pet.";
 function WhatsAppFloat() {
   return /* @__PURE__ */ React.createElement(React.Fragment, null, /* @__PURE__ */ React.createElement(
     "a",
